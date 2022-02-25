d250169c39a822523e1195c72258ffe6f425979f
Luiz Lopes <luizlopes12@outlook.com>
2022-02-25 13:04:25 -0300

    ajustes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,46 @@
-//Add ids nos botoes de 1 a 4
-//selecionar os botões
-//interceptar evento de click
-//recuperar os ids selecionados
+//1 ADICIONAR IDS NOS BOTÕES **
+//1...4 ***
+//2 SELECIONAR OS BOTÕES
+//3 INTERCEPTAR O EVENTO DE CLIQUE 
+//4 RECUPERAR O ID ADIONADO 
 
-const lista = document.querySelector('.secaoDetalhesConta__opcoesTransacoes')
-lista.addEventListener('click', identificarOpcoes)
 
-const identificarOpcoes = (event) =>{
-    console.log(event.target.id)
+//SELECIONANDO UL(LISTA)
+const listaOpcoes = document.querySelector(".secaoDetalhesConta__opcoesTransacoes")
+
+//ADICIONANDO ESCUTADOR DE EVENTOS
+listaOpcoes.addEventListener("click", indentificarOpcoes)
+
+//EXECUTANDO AÇÃO DEPOIS DO CLIQUE 
+function indentificarOpcoes(event){
+    
+    //IDENTIFICANDO ONDE OCORREU O CLIQUE
+    const elemento  = event.target
+
+    //VERIFICANDO SE É UM LI 
+    if(elemento.tagName == "LI"){
+
+        //RECUPERANDO O ID
+        const id = elemento.id
+        
+        //SELECIONANDO UMA SEÇÃO UTILIZANDO O ID(ESCOLHA DO USUÁRIO)
+        const secaoEscolha  = document.querySelector(`div[data-id="${id}"]`)
+
+        //REMOCENDO CLASSE MOSTRAR
+        removeClasseMostrar()
+        
+        //ADICIONAR UMA CLASSE 
+        secaoEscolha.classList.add("mostrar")
+    }
+}
+
+//FUNÇÃO PARA REMOVER AS CLASSES DAS DIVS  
+function removeClasseMostrar(){
+    const divs = document.querySelectorAll(".secaoTransacao .container div")
+    
+    for(let i = 0; i < divs.length; i++){
+
+        divs[i].classList.remove("mostrar")
+
+    }
 }
